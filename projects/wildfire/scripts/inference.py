6dd3ace134db080738aa3fb6ec578b22cf5c3b58
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2024-03-26 22:01:29 +0900

    Seperate to make_submission.py

--- projects/wildfire/scripts/inference.py
+++ projects/wildfire/scripts/inference.py
@@ -7,8 +7,6 @@ from mmengine.config import Config
 from mmengine.registry import DATASETS, MODELS
 from mmengine.runner.checkpoint import load_checkpoint
 
-from projects.wildfire.datasets import TestDataset
-
 
 @torch.no_grad()
 def main(config_path: Path, ckpt_path: Path):
@@ -70,49 +68,6 @@ def main2():
     print(total_iou)
 
 
-@torch.no_grad()
-def main3(config_path: Path, ckpt_path: Path):
-
-    cfg = Config.fromfile(config_path)
-
-    device = torch.device("cuda")
-    model = MODELS.build(cfg.model).to(device)
-    load_checkpoint(
-        model, str(ckpt_path), map_location="cpu", strict=True, revise_keys=[(r"module\.", "")]
-    )
-
-    dataset = TestDataset(input_chs=[0, 1, 2, 3, 4, 5, 6])
-
-    model.eval()
-
-    preds = []
-    img_paths = []
-    for idx, x in enumerate(dataset):
-        img_x = torch.tensor(x["img"]).to(device)
-        img_x = torch.stack([img_x, img_x.flip(1), img_x.flip(2), img_x.flip([1, 2])])
-
-        y_pred, _ = model.forward(mode="predict", img=img_x, mask=None)
-
-        y_pred[1] = y_pred[1].flip(1)
-        y_pred[2] = y_pred[2].flip(2)
-        y_pred[3] = y_pred[3].flip([1, 2])
-        y_pred = y_pred.mean(0)
-
-        preds.append(y_pred.squeeze().cpu().numpy())
-        img_paths.append(x["img_path"])
-
-    preds = np.stack(preds)
-    preds = (preds > 0.5).astype(np.uint8)
-
-    y_pred_dict = {}
-    for img_path, pred in zip(img_paths, preds, strict=True):
-        y_pred_dict[img_path.name] = pred
-
-    import joblib
-
-    joblib.dump(y_pred_dict, "./y_pred.pkl")
-
-
 def main4():
     from torchmetrics.classification import BinaryJaccardIndex
 
@@ -151,6 +106,5 @@ def main4():
 
 
 if __name__ == "__main__":
-    # typer.run(main)
+    typer.run(main)
     # main2()
-    typer.run(main3)
